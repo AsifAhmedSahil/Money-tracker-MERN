595183327631183e48cc4c4042cb4db4949ec65c
EngineerSahil <77582744+EngineerSahil@users.noreply.github.com>
2023-01-06 15:53:59 +0600

    database done

--- server/index.js
+++ server/index.js
@@ -1,8 +1,37 @@
 const express = require("express");
+const cors = require('cors');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.quaequt.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+        const expenseCollection = client.db("moneyTracker").collection("cost")
+
+        app.post("/expense",async(req,res)=>{
+            const expense = req.body;
+            const result = await expenseCollection.insertOne(expense);
+            res.send(result);
+        })
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(err => console.log(err))
 
 
 app.get("/",(req,res)=>{
